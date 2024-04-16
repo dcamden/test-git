c779d9137f463a321b4ab1386a6cc2ee9f0b8ae9
dcamden <167147844+dcamden@users.noreply.github.com>
2024-04-15 20:04:51 -0500

    Delete Python Crash Course directory

--- Python Crash Course/practice.py
+++
@@ -1,7 +0,0 @@
-# grocery_list = ['apple', 'bananas', 'spinach', 'blueberries', 'carrots', 'watermelon']
-
-# letter_check = input("What letter would you like to check for?")
-# #number after item checks for letter in the position of word. 0 is first, 1 is second, etc]
-# for item in grocery_list:
-#     if item[1] == letter_check:
-#         print(item)
